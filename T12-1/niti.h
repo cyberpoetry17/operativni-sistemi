c012ef7d37ea35ff42db18042d8c874f51e8a145
sajberPoezija <44535341+cyberpoetry17@users.noreply.github.com>
2019-03-09 16:08:04 +0100

    Add files via upload

---
+++ T12-1/niti.h
@@ -0,0 +1,47 @@
+#ifndef NITI_H
+#define NITI_H
+
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+// Implementacija sabiranja korespondentnih elemenata celih vektora A i B pokretanjem onoliko niti koliko ima elemenata u vektorima
+//
+// a - Vektor A
+// b - Vektor B
+//
+// Rezultat izvršavanja treba da bude vector<int> koji sadrži rezultat sabiranja korespondentnih elemenata.
+//
+vector<int> izracunaj(vector<int>& a, vector<int>& b)
+{
+    // Implementirati ...
+    int velicina=a.size();
+    vector<int> rezultat(velicina);
+    //svaka nit obradjuje po jedan deo ovog niza rezultat
+
+    thread niti[velicina];
+
+    //pravljenje niti
+
+
+    for(int i=0;i<velicina;i++){
+
+        niti[i]=thread(saberiPar,a.begin()+i,b.begin()+i,rezultat.begin()+i);
+
+
+    }
+
+    for(int i=0;i<velicina;i++){
+
+        niti[i].join();
+
+    }
+
+
+
+ return rezultat;
+
+}
+
+#endif // NITI_H
