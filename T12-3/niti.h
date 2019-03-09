3114c000aaadd2598bf041b6f9fc1eb530b56e85
sajberPoezija <44535341+cyberpoetry17@users.noreply.github.com>
2019-03-09 16:14:00 +0100

    Add files via upload

---
+++ T12-3/niti.h
@@ -0,0 +1,31 @@
+#ifndef NITI_H
+#define NITI_H
+
+#include <thread>
+#include <list>
+
+using namespace std;
+
+// Implementacija obrtanja redosleda elemenata u listi pokretanjem jedne nove niti
+//
+// ulazna  - Ulazna lista, koja se obrađuje
+//
+// Rezultat je rezultujuća lista (popunjena elementima u obrnutom redosledu)
+//
+list<int> izracunaj(const list<int>& ulazna)
+{
+    // Implementirati ...
+
+
+    list<int> izlazna;
+
+    thread nit;
+
+
+    nit=thread(obrniRedosled,ulazna,ref(izlazna));
+    nit.join();
+
+    return izlazna;
+}
+
+#endif // NITI_H
