ee1d73e3111395ad918f6c8a42045c212f810ea1
sajberPoezija <44535341+cyberpoetry17@users.noreply.github.com>
2019-03-10 12:43:58 +0100

    Add files via upload

---
+++ T12-5/niti.h
@@ -0,0 +1,25 @@
+#ifndef NITI_H
+#define NITI_H
+
+#include <thread>
+#include <map>
+
+using namespace std;
+
+// Implementacija pokretanja dodatne niti koja treba da napravi
+//novi rečnik (koristeći funkciju "napraviRecnik")
+//
+// brojNiti - Broj niti koje je potrebno startovati
+map<string, string> izracunaj(map<string, string> engSrp)
+{
+   map<string,string> srpEng;
+//mape moramo po referenci da sutamo
+    thread nit;
+    nit=thread(napraviRecnik,ref(engSrp),ref(srpEng));
+    nit.join();
+return srpEng; //mora da vrati nesto
+
+
+}
+
+#endif // NITI_H
