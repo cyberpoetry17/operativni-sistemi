dc5b56a3f814d7218784e9792a08566f505114d9
sajberPoezija <44535341+cyberpoetry17@users.noreply.github.com>
2019-03-10 12:47:02 +0100

    Add files via upload

---
+++ T12-8/cenovnik.h
@@ -0,0 +1,35 @@
+#ifndef CENOVNIK_H
+#define CENOVNIK_H
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+void nadjiPovoljno(map<string, double> a, map<string, double> b, map<string, double> &jeftino)
+{
+
+  map<string ,double>::iterator it1;
+  string s1,s2;
+
+
+  for(it1=a.begin();it1!=a.end();it1++){
+
+      jeftino[it1->first]=min(it1->second,b[it1->first]);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+#endif // CENOVNIK_H
---
+++ T12-8/niti.h
@@ -0,0 +1,20 @@
+#ifndef NITI_H
+#define NITI_H
+
+#include <thread>
+
+using namespace std;
+
+map<string, double> izracunaj(map<string, double>& a, map<string, double>& b)
+{
+
+    map<string, double> rezultat;
+
+    thread nit(nadjiPovoljno,a,b,ref(rezultat));
+    nit.join();
+
+    return rezultat;
+
+}
+
+#endif // NITI_H
